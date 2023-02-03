5eff7e1a7c2a5802399977434ec5eade4b6a8bc4
Aman Gupta <aag9131@nyu.edu>
2023-02-03 14:37:37 -0500

    api updated

--- backend/apis.py
+++ backend/apis.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from fastapi.middleware.cors import CORSMiddleware
 import os
 import time
 import csv
@@ -6,6 +7,25 @@ import json
 from recommend_playlist import *
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "localhost:3000"
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/", tags=["root"])
+async def read_root() -> dict:
+    return {"message": "Welcome to playlist generator."}
+
 @app.get("/input")
 def start(text: str):
     os.system("recommend_model.py input -t \""+text+"\"")
@@ -15,7 +35,7 @@ def start(text: str):
         songs = [{k: v for k, v in row.items()}
         for row in csv.DictReader(f, skipinitialspace=True)]
     final = json.dumps(songs,indent = 2)
-    return final
+    return Response(content=final, media_type="application/json")
 
 
 
